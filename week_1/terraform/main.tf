daf2a4280e3587e2e24a42b2689eca0064cb9a6a
Fiddler <nghhaidang@gmail.com>
2024-01-21 23:13:17 +0700

    terraform homework

---
+++ week_1/terraform/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+    # credentials = "./week_1/terraform/my-creds.json"
+    project = var.project
+    region  = var.region
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+    # Optional, but recommended settings:
+  # storage_class = "STANDARD"
+  # uniform_bucket_level_access = true
+
+  # versioning {
+  #   enabled     = true
+  # }
+
+  lifecycle_rule {
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+    condition {
+      age = 1  // days
+    }
+  }
+}
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  project    = var.project
+  location   = var.location
+}
