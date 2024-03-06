f06a6cc2d9f155f78805a5300500d659887b7ae3
Dang Nguyen <nghhaidang@gmail.com>
2024-03-06 16:38:44 +0700

    adjuting pipelines

--- project/mage_pipeline/OPEN_WEATHER_PIPELINE/data_loaders/locations.py
+++ project/mage_pipeline/OPEN_WEATHER_PIPELINE/data_loaders/locations.py
@@ -2,18 +2,19 @@ import io
 import pandas as pd
 import requests
 import json
+from pandas import DataFrame
+from typing import Union
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from os import path
 
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
-@data_loader
-def get_locations(*args, **kwargs):
-    """
-    Template for loading data from API
-    """
-    cities = ['An Giang', 'Ba Ria - Vung Tau', 'Bac Lieu', 'Bac Giang', 'Bac Kan', 'Bac Ninh', 
+cities = ['An Giang', 'Ba Ria - Vung Tau', 'Bac Lieu', 'Bac Giang', 'Bac Kan', 'Bac Ninh', 
           'Ben Tre', 'Binh Duong', 'Binh Dinh', 'Binh Phuoc', 'Binh Thuan', 'Ca Mau', 'Cao Bang', 
           'Can Tho', 'Da Nang', 'Dak Lak', 'Dak Nong', 'Dien Bien', 'Dong Nai', 'Dong Thap', 'Gia Lai', 
           'Ha Giang', 'Ha Nam', 'Ha Noi', 'Ha Tinh', 'Hai Duong', 'Hai Phong', 'Hau Giang', 'Hoa Binh', 
@@ -23,6 +24,21 @@ def get_locations(*args, **kwargs):
           'Soc Trang', 'Son La', 'Tay Ninh', 'Thai Binh', 'Thai Nguyen', 'Thanh Hoa', 'Thua Thien Hue', 
           'Tien Giang', 'Tra Vinh', 'Tuyen Quang', 'Vinh Long', 'Vinh Phuc', 'Yen Bai']
 
+table_id = 'pelagic-bonbon-387815.de_zoomcamp_pj.locations'
+config_path = path.join(get_repo_path(), 'io_config.yaml')
+config_profile = 'default'
+
+def check_table_exist() -> Union[DataFrame|bool]:
+    # get data from BQ to check if it has data
+    query = f'select * from pelagic-bonbon-387815.de_zoomcamp_pj.locations'
+    df = BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).load(query)
+    if df.shape[0] != len(cities):
+        print(f'empty Df')
+        return pd.DataFrame()
+    else:
+        return df
+
+def get_locations():
     locations = []
 
     # loop over cities to get their coordinates
@@ -37,7 +53,18 @@ def get_locations(*args, **kwargs):
         }
         locations.append(location)
     df = pd.DataFrame.from_records(locations)
+    return df
 
+
+@data_loader
+def locations(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    df = check_table_exist()
+
+    if df.empty:
+        df = get_locations()
     return df
 
 
